1f95d04383a62c277232b82724cc2817e3ffd7d5
Legoburner73 <legoplaysmcmail@gmail.com>
2016-07-25 13:14:10 +0100

    Update Main.php

--- ExamplePlugin/src/legoburner73/ExamplePlugin/Main.php
+++ ExamplePlugin/src/legoburner73/ExamplePlugin/Main.php
@@ -4,10 +4,19 @@ namespace legoburner73\ExamplePlugin;
 
 use pocketmine\plugin\PluginBase as Base;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends Base implements Listener
 {
   public function onEnable() {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "test"){
+            $sender->sendMessage("Yay.. command worked as normal user!");
+            return true;
+        }
+        return false;
+    }
 }
